0b21ac31895a7ee0d4d22309549d03f813879731
Rino-program <kihiro.0422.max@gmail.com>
2026-08-27 11:35:11 +0900

    v0.1.1

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/lib/mailer.php';
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 if (in_array($origin, (array) config('allowed_origins', []), true)) { header("Access-Control-Allow-Origin: $origin"); header('Access-Control-Allow-Credentials: true'); }
-header('Access-Control-Allow-Headers: Content-Type, Authorization'); header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Authorization'); header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') exit;
 
 $method = $_SERVER['REQUEST_METHOD']; $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
